910153d6a66c6058a69f1e562d05194497c7566a
Justin Boshoven <jsbosh@gmail.com>
2014-10-17 10:52:38 -0500

    Help.txt is now an embedded resource so that the calculator can be a single executable.

--- Calculator 3/Windows/Help.cs
+++ Calculator 3/Windows/Help.cs
@@ -43,7 +43,9 @@ namespace Calculator.Windows
 
 		private void Help_Load(object sender, EventArgs e)
 		{
-			label1.Text = File.ReadAllText("Help.txt");
+			using (var stream = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Calculator.Help.txt")))
+				label1.Text = stream.ReadToEnd();
+				
 			var mySize = label1.Size;
 			mySize.Width += label1.Location.X * 2;
 			mySize.Height += label1.Location.Y * 2;
